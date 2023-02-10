cf9004fc605becf9cfce9849ca45cd973ff852f9
玉 段 <211292753@qq.com>
2023-02-10 12:27:03 +0800

    test1

--- src/main/java/veryRed/Main.java
+++ src/main/java/veryRed/Main.java
@@ -2,6 +2,7 @@ package veryRed;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("you!");
+        System.out.printf("asdasdasd!");
+        System.out.printf("aaa!");
     }
 }
