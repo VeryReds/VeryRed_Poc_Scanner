0dd8e5abda6cedfe0df6f7dae11c27582b8c54c9
玉 段 <211292753@qq.com>
2023-02-10 12:39:29 +0800

    测试提交

--- src/main/java/veryRed/Main.java
+++ src/main/java/veryRed/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("asdasdasd!");
         System.out.printf("aaa!");
-        System.out.printf("aaa!");
     }
 }
