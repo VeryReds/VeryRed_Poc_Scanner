25c975b74881367cb0bb2ffc24e7c5dca8d82e15
玉 段 <211292753@qq.com>
2023-02-12 18:39:15 +0800

    添加url文件为空判断

--- src/main/java/veryRed/Main.java
+++ src/main/java/veryRed/Main.java
@@ -7,8 +7,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
-        for (String s : Cmd.get_url(args)) {
+        String[] urls = Cmd.get_url(args);
+        if(urls.length == 0){
+            System.out.println("url文件为空");
+            System.exit(-1);
+        }
+        for (String s : urls) {
             System.out.println(s);
         }
     }
--- src/main/java/veryRed/tools/File.java
+++ src/main/java/veryRed/tools/File.java
@@ -25,6 +25,6 @@ public class File {
             e.printStackTrace();
         }
 
-        return null;
+        return urls;
     }
 }
